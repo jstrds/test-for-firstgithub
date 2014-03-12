60848f93059b971504e073577b32a264c02a9127
Justin Richardson <richardson@rivendell@hcs.ufl.edu>
2014-03-12 11:01:23 -0400

    Version:0.0.2 Description: Added hello world c file
     Committer: Justin Richardson <richardson@rivendell@hcs.ufl.edu>
     On branch master
     Changes to be committed:
    	new file:   src/hello_world.c

---
+++ src/hello_world.c
@@ -0,0 +1,18 @@
+//=========================================================%
+//F5 Benchmarking Hello World on Github                    %
+//Author: Justin Richardson                                %
+//Version: 00.00.002                                       %
+//Description: This document is a test for the             %
+//    functionality of GitHub for our applications         %
+//=========================================================%
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello World\n");
+
+    return 0;
+
+}
+
+//EoF
